28cee9db6998ad09e8dc1baf877e26a269088d73
vexojs <admin@mavibase.com>
2026-09-06 20:19:30 +0100

    fix: load Pillbox interior archetypes

--- resources/[NanoMLO]/pillbox_hospital/fxmanifest.lua
+++ resources/[NanoMLO]/pillbox_hospital/fxmanifest.lua
@@ -6,3 +6,11 @@ description 'Pillbox Hospital MLO map resource'
 version '1.0.0'
 
 this_is_a_map 'yes'
+
+files {
+  'stream/ytyp/gabz_pillbox_dlc_int.ytyp',
+  'stream/ytyp/gabz_pillbox_int.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/gabz_pillbox_dlc_int.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/gabz_pillbox_int.ytyp'
