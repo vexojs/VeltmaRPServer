ecef192fae6f266623d75c20e5100c31c2fdcd88
vexojs <admin@mavibase.com>
2026-09-08 12:48:27 +0100

    fix: deploy butterfly wing stream assets

--- resources/[veltma]/veltma-clothing/fxmanifest.lua
+++ resources/[veltma]/veltma-clothing/fxmanifest.lua
@@ -18,9 +18,13 @@ server_script 'dist/server/index.js'
 client_script 'dist/client/index.js'
 
 files {
+  'stream/*.ydd',
+  'stream/*.ytd',
+  'stream/*.ymt',
   'stream/**/*.ydd',
   'stream/**/*.ytd',
   'stream/**/*.ymt'
 }
 
+data_file 'SHOP_PED_APPAREL_META_FILE' 'stream/*.ymt'
 data_file 'SHOP_PED_APPAREL_META_FILE' 'stream/**/*.ymt'
